e85ed622a4581e4a387fe3822c4c14045a1ee879
Tony Santos <tony@notjustprettypictures.com>
2020-01-17 17:05:40 -0800

    turned direct link portfolio pages into generated pages

--- src/portfolio/portfolio.11tydata.js
+++ src/portfolio/portfolio.11tydata.js
@@ -3,9 +3,9 @@ module.exports = {
     {
       title: 'Leadership',
       sidebar: [
-        "Hi there! Thanks for taking the time to look at my stuff. I know this is one of a stack of portfolios you're looking at inbetween meetings or while you drink your coffee, before you get back into all the other non - hiring stuff on your plate.So let's get right to it: The following are three case summaries of teams I've lead at three very different companies working on three very different products.",
-        "In addition to these leadership case studies you may also want to check out my writing < a href = \"#\" > about design</a > and < a href = \"#\" > code as a design tool.</a > Maybe you're interested in <a href=\"#\">individual design work?</a>",
-        "Thank you for taking the time to stop by. I hope I've piqued your interest and you're dying to have a more in-depth conversation soon.",
+        "Hi there! Thanks for taking the time to look at my stuff. I know this is one of a stack of portfolios you're looking at between meetings or while you drink your coffee. So let's get right to it: The following are three case summaries of teams I've lead at three very different companies, on three very different products.",
+        "In addition to these leadership case studies you may also want to check out my writing <a href = \"/words-i-said/\"> about design</a> and <a href = \"/code-like-a-designer/\" > code as a design tool.</a> Maybe you're interested in <a href=\"/portfolio/individual/\">individual design work?</a>",
+        "Thanks stopping by, let's talk soon!",
       ],
       items: [
         {
@@ -21,16 +21,16 @@ module.exports = {
         {
           title: 'T-Mobile',
           thumbnail: '',
-          desc: 'Leading a team of early career designers and researchers updating legacy customer support systems for touch screens and modern devices.',
+          desc: 'Leading a team of early career designers, researchers, and developers updating legacy customer support systems for touch screens and modern devices.',
         },
       ],
     },
     {
       title: 'Individual',
       sidebar: [
-        "Hi there! Thanks for taking the time to look at my stuff. I know this is one of a stack of portfolios you're looking at inbetween meetings or while you drink your coffee, before you get back into all the other non - hiring stuff on your plate. So let's get right to it: The following are three case summaries of projects I've worked on at three very different companies working on at three very different companies.",
-        "In addition to these IC case studies you may also want to check out my writing < a href = \"#\" > about design</a > and < a href = \"#\" > code as a design tool.</a > Maybe you're interested in <a href=\"#\">leadership work?</a>",
-        "Thank you for taking the time to stop by. I hope I've piqued your interest and you're dying to have a more in-depth conversation soon.",
+        "Hi there! Thanks for taking the time to look at my stuff.I know this is one of a stack of portfolios you're looking at between meetings or while you drink your coffee. So let's get right to it: The following are three case summaries of projects I've worked on at three very different companies.",
+        "In addition to these IC case studies you may also want to check out my writing <a href = \"/words-i-said/\"> about design</a> and <a href = \"/code-like-a-designer/\" > code as a design tool.</a> Maybe you're interested in <a href=\"/portfolio/leadership/\">leadership work?</a>",
+        "Thanks stopping by, let's talk soon!",
       ],
       items: [
         {
